19841147683ffcf9ca6671c97c28262c6cd21e87
405 <405@K5>
2025-03-12 10:05:19 +0100

    Add project files.

---
+++ Plat-dot-NET-33INF-SSI-SP_LAB_01_Julia_Grzeskowiak/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Plat_dot_NET_33INF_SSI_SP_LAB_01_Julia_Grzeskowiak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Zadanie_03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zadanie_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
