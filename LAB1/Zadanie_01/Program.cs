3d10d5d18b93b146aae54885c14fcbd60fe159ad
Julia Grzeskowiak <109239@g.elearn.uz.zgora.pl>
2025-03-18 21:14:07 +0100

    cleaning repo, adding folders

---
+++ LAB1/Zadanie_01/Program.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Windows.Forms;
+
+//namespace Zadanie_01
+//{
+//    internal static class Program
+//    {
+//        [STAThread]
+//        private static void Main()
+//        {
+//            Application.EnableVisualStyles();
+//            Application.SetCompatibleTextRenderingDefault(false);
+//            Application.Run(new MainForm());
+//            //pobrano grę Matching Game i skompilowano ją
+//        }
+//    }
+//}
---
+++ LAB1/Zadanie_03/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Zadanie_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Baska";
+            int number = 65;
+            Console.WriteLine("Name:" + name + " its number:" + number);
+            Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+        }
+
+        public static System.Collections.IDictionary GetEnvironmentVariables()
+        {
+            return Environment.GetEnvironmentVariables();
+        }
+    }
+}
