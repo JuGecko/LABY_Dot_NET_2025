d3375b0825b2ec730a5b0bbebe5c5f5c6f1ad352
JuGecko <grzeskowiak.pocztaog@gmail.com>
2025-03-26 10:46:33 +0100

    starting ex 3

--- Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_02/Zadanie_03/Form1.Designer.cs
+++ Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_02/Zadanie_03/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Zadanie_03
+﻿//program, który zaprezentuje możliwości klasy MessageBox (a dokładniej metody Show) z przestrzeni System.Windows
+//MessageBox
+namespace Zadanie_03
 {
     partial class Form1
     {
@@ -20,6 +22,8 @@
             base.Dispose(disposing);
         }
 
+
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,12 +32,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(71, 54);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 19);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "opcja 1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(71, 101);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(63, 19);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "opcja 2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(71, 148);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(63, 19);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "opcja 3";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(71, 196);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(63, 19);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "opcja 4";
+            radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(640, 282);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
     }
+
 }
--- Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_02/Zadanie_03/Form1.cs
+++ Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_02/Zadanie_03/Form1.cs
@@ -6,5 +6,25 @@ namespace Zadanie_03
         {
             InitializeComponent();
         }
+        //wyswietla pole komunikatu
+        public sealed class MessageBox;
+        //https://learn.microsoft.com/en-us/dotnet/api/system.windows.messagebox?view=windowsdesktop-7.0
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
