fe40e2c2aa2b4ef6d6dc0a48c065357d894308bc
Julia Grzeskowiak <109239@g.elearn.uz.zgora.pl>
2025-04-22 12:43:37 +0200

    zadanie 2 adr ip zrobione

--- Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_03/Zadanie_02/Form1.Designer.cs
+++ Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_03/Zadanie_02/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBoxE = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(29, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(61, 27);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 20);
+            label1.TabIndex = 1;
+            label1.Text = "adres IP";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 135);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 20);
+            label2.TabIndex = 3;
+            label2.Text = "e-mail";
+            // 
+            // textBoxE
+            // 
+            textBoxE.Location = new Point(29, 169);
+            textBoxE.Name = "textBoxE";
+            textBoxE.Size = new Size(309, 27);
+            textBoxE.TabIndex = 2;
+            textBoxE.TextChanged += textBoxE_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(96, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(61, 27);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(163, 61);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(61, 27);
+            textBox3.TabIndex = 5;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(230, 61);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(61, 27);
+            textBox4.TabIndex = 6;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(364, 61);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "Weryfikuj";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(364, 169);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 8;
+            button2.Text = "Weryfikuj";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(498, 312);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBoxE);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxE;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
     }
 }
--- Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_03/Zadanie_02/Form1.cs
+++ Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_03/Zadanie_02/Form1.cs
@@ -1,3 +1,6 @@
+//podaæ program dla konsoli do weryfikacji adresu IP w formacie:
+//XXX.XXX.XXX.XXX,
+//gdzie X to cyfra oraz e-mail za pomoc¹ wyra¿enia regularnego.
 namespace Zadanie_02
 {
     public partial class Form1 : Form
@@ -5,6 +8,61 @@ namespace Zadanie_02
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool IsValidOctet14(int value)
+        {
+            return value >= 1 && value <= 254;
+        }
+        private bool IsValidOctet23(int value)
+        {
+            return value >= 0 && value <= 255;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int octet1 = int.Parse(textBox1.Text);
+                int octet2 = int.Parse(textBox2.Text);
+                int octet3 = int.Parse(textBox3.Text);
+                int octet4 = int.Parse(textBox4.Text);
+
+                if (IsValidOctet14(octet1) && IsValidOctet14(octet4) && IsValidOctet23(octet2) && IsValidOctet23(octet3))
+                {
+                    MessageBox.Show("Adres IP jest poprawny.", "Weryfikacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Adres IP jest niepoprawny. Oktety 1 i 4 musza byc w zakresie 001-254, a oktety 2 i 3 w zakresie 000-255.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Podaj poprawny adres IP.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //email
+        private void textBoxE_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
