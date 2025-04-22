36def9ad140c0d98f4f0ef4dc606b3a775f1dd7a
Julia Grzeskowiak <109239@g.elearn.uz.zgora.pl>
2025-04-22 21:42:49 +0200

    zrobione LAB3 bez zad3

---
+++ Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_03/Zadanie_07/MyCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_07
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class MyCollection : IEnumerable<int>
+    {
+        private List<int> _items = new List<int>();
+
+        public void Add(int item)
+        {
+            _items.Add(item);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            // Delegate to the List's enumerator
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Required for non-generic IEnumerable
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Plat-dot-NET-33INF-GRP-B-SSI-SP_LAB_03/Zadanie_07/Program.cs
@@ -0,0 +1,23 @@
+﻿//podać definicję własnej klasy zgodnej z pętlą typu foreach
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Zadanie_07;
+
+class Program
+{
+    static void Main()
+    {
+        MyCollection numbers = new MyCollection();
+        numbers.Add(7);
+        numbers.Add(3);
+        numbers.Add(5);
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
+
+
